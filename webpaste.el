830704a7282b6af1682ce587d8bf3a9fb03b9a06
Elis Axelsson <elis.axelsson@gmail.com>
2016-09-18 22:08:09 +0200

    Added base lisp file

---
+++ webpaste.el
@@ -0,0 +1,37 @@
+;;; webpaste.el --- Paste to pastebin-like services
+
+;; Copyright (c) 2016 Elis Axelsson
+
+;; Author: Elis "etu" Axelsson
+;; URL: https://github.com/etu/webpaste.el
+;; Package-Version: 0
+;; Version: 0.0.1
+;; Keywords: convenience, webpaste
+
+;;; Commentary:
+
+;; This mode will allow the user to paste parts or whole buffers
+;; to pastebin-like services.
+
+;;; License:
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
+;;; Code:
+
+(provide 'webpaste)
+
+;;; webpaste.el ends here
