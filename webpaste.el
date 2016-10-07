ac1f7720a6281e9bf8f8dcbb12d9c47ed25ea20b
Elis Axelsson <elis.axelsson@gmail.com>
2016-10-07 23:18:01 +0200

    Commentary style

--- webpaste.el
+++ webpaste.el
@@ -12,8 +12,8 @@
 ;;; Commentary:
 
 ;; This mode allows to paste whole buffers or parts of buffers to
-;; pastebin-like services. It supports more than one service and will
-;; failover if one service fails. More services can easily be added
+;; pastebin-like services.  It supports more than one service and will
+;; failover if one service fails.  More services can easily be added
 ;; over time and prefered services can easily be configured.
 
 ;;; License:
