0c814bc31208d994359e786c28338b46ce31c01e
Elis Axelsson <elis.axelsson@gmail.com>
2017-06-04 11:40:45 +0200

    Added integration test for sprunge.us

--- tests/integration/test-webpaste-providers.el
+++ tests/integration/test-webpaste-providers.el
@@ -49,7 +49,20 @@
             :to-equal
             (make-spy-context :current-buffer (current-buffer)
                               :args '("http://ix.io/whJ")
-                              :return-value nil)))))
+                              :return-value nil))))
+
+
+ (it
+  "can paste with sprunge.us"
+
+  (let ((provider (cadr (assoc "sprunge.us" webpaste-providers-alist))))
+    (funcall
+     provider
+     ";; This is a build artifact made from an integration test for https://github.com/etu/webpaste.el"
+     :sync t)
+
+    (expect (spy-calls-count 'webpaste-return-url) :to-equal 1)
+    (expect (spy-calls-count 'webpaste-paste-text) :to-equal 0))))
 
 
 ;;; test-webpaste-providers.el ends here
