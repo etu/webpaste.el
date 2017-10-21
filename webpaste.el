aed3e00b6332a068d53ce482f5139a95c3dcd245
Elis Hirwing <elis@hirwing.se>
2017-10-21 23:07:53 +0200

    Bump version for new release

--- webpaste.el
+++ webpaste.el
@@ -4,8 +4,8 @@
 
 ;; Author: Elis "etu" Axelsson
 ;; URL: https://github.com/etu/webpaste.el
-;; Package-Version: 1.5.0
-;; Version: 1.5.0
+;; Package-Version: 2.0.0
+;; Version: 2.0.0
 ;; Keywords: convenience, comm, paste
 ;; Package-Requires: ((emacs "24.4") (request "0.2.0") (cl-lib "0.5"))
 
