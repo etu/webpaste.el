356e54f6ce65d0027a05dbc5fb0b05ee78cf7490
Elis Hirwing <elis@hirwing.se>
2018-08-15 19:45:57 +0200

    Tests: Add unit test for webpaste--get-shebang-lang-mode

---
+++ tests/unit/test-webpaste-get-language.el
@@ -0,0 +1,32 @@
+;;; test-webpaste-get-language.el --- Tests for webpaste
+;;; Commentary:
+;;; Code:
+
+(load "tests/load-undercover.el")
+(require 'webpaste)
+
+(describe
+ "Get language for buffer"
+
+ ;; Setup the current buffer with a bash script
+ (before-each
+  (insert "#!/usr/bin/env bash")
+  (newline)
+  (insert "echo 'Hello'"))
+
+ ;; Kill the contents of the current buffers bash script
+ (after-each
+  (kill-region (point-min) (point-max)))
+
+ (it
+  "can get the language by shebang"
+
+  ;; Setup. Let the mode be fundamental-mode.
+  (let ((major-mode 'fundamental-mode))
+    ;; Do test. And expect the matched mode to be bash-mode, since the buffer
+    ;; contains a bash shebang.
+    (expect (webpaste--get-shebang-lang-mode)
+            :to-equal
+            'bash-mode))))
+
+;;; test-webpaste-get-language.el ends here
