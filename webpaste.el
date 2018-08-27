22f64b77989ac6c63d7ea4b74625fd463b527442
Elis Hirwing <elis@hirwing.se>
2018-08-27 18:59:36 +0200

    undercover: Fix up undercover reporting
    Suggestion from Undercover contributor/member here:
    https://github.com/undercover-el/undercover.el/issues/42#issuecomment-557700027

--- webpaste.el
+++ webpaste.el
@@ -205,7 +205,7 @@ precalculated, and also available both for pre and post request access.")
                  (webpaste--paste-text text))))
 
 
-(cl-defun webpaste--providers-error-lambda-no-failover (&rest)
+(cl-defun webpaste--providers-error-lambda-no-failover (&rest _)
   "Predefined error callback for providers that shouldn't do failover."
   (cl-function (lambda (&key error-thrown &allow-other-keys)
                  (message "Got error: %S" error-thrown))))
